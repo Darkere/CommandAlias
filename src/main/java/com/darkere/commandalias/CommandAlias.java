1c5bf33b7a08c4d5de2161022e1767440f21f824
Darkere <Dark-277@web.de>
2020-07-28 23:35:49 +0200

    fix multiplayer compat visual

--- src/main/java/com/darkere/commandalias/CommandAlias.java
+++ src/main/java/com/darkere/commandalias/CommandAlias.java
@@ -4,7 +4,11 @@ import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -16,6 +20,7 @@ public class CommandAlias
 
     public CommandAlias() {
         MinecraftForge.EVENT_BUS.register(this);
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
     }
 
 
